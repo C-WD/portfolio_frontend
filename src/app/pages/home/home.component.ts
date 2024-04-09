211207d983734dc6ea51bb600957c3dee6895ba3
AbmaelSouza <abmaelpsouza@gmail.com>
2024-04-09 10:04:42 -0300

    aueba, começou

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
-export class AppComponent {
+export class HomeComponent {
   title = 'portfolio_frontend';
   cardList = Array.from({ length: 100 }).fill(null);
 }
