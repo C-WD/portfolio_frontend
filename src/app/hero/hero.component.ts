9d5c729570dd7cae4f63e374b96e65aa26912a97
AbmaelSouza <abmaelpsouza@gmail.com>
2024-05-05 18:17:28 -0300

    adicionado dados pelo script no hero

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+hero = {
+  topText: 'Branding | Image making',
+  titleText:'My awesome portfolio',
+  obsText:'And I made it myself! Yes. In Figma with Anima',
+  heroImg:{
+    src:'assets/hero/hero-image.png',
+    alt:"hero banner illustration with some balls and exotic shapes on the inside of them like child's toys"
+  }
+}
+
 }
